f42862f58ee0f9edc42c8bb00f11627812c634d1
Shivam Kumar <shivam.kumar@example.com>
2026-06-25 07:44:03 +0530

    Restore outDir config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
+  build: {
+    outDir: '../Workden4.0',
+    emptyOutDir: true
+  },
   plugins: [
     react(),
   ],
